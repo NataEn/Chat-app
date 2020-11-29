390c9f3d09f19517add240b77d6e4b4e5d595fea
natalie <natalie@enkin.co.il>
2020-11-29 13:37:30 +0200

    changes to stabilize socket opening, inprocess

--- server/socketio.js
+++ server/socketio.js
@@ -1,11 +1,16 @@
-const server = require("./bin/server");
 const socketio = require("socket.io");
-const io = socketio(server);
-io.on("connection", (socket) => {
-  console.log("A new connection established");
-  socket.on("disconnect", () => {
-    console.log("user has left");
-  });
-});
 
-module.exports = io;
+const appio = (server) => {
+  const io = socketio(server);
+  io.on("connection", (socket) => {
+    console.log("A new connection established");
+    socket.on("join", () => {
+      console.log("someone joined a chat room");
+    });
+    socket.on("disconnect", () => {
+      console.log("user has left");
+    });
+  });
+};
+
+module.exports = appio;
