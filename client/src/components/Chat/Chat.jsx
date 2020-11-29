dec50df94a1f0264ede74310ae2d2ab8554731b0
natalie <natalie@enkin.co.il>
2020-11-29 12:14:57 +0200

    client-adding socket opening

--- client/src/components/Chat/Chat.jsx
+++ client/src/components/Chat/Chat.jsx
@@ -3,17 +3,21 @@ import {useLocation} from "react-router-dom"
 import queryString from 'query-string'
 import io from 'socket.io-client'
 
+let socket;
+
 const Chat=()=>{
     const location = useLocation();
     const [name,setName]=useState('')
     const [room,setRoom]=useState('')
+    const ENDPOINT='localhost:5000'
     
    useEffect(()=>{
     const {name,room}= queryString.parse(location.search)
+    socket=io(ENDPOINT)
     setName(name);
     setRoom(room)
-    console.log(name,room)
-   })
+    socket.emit('join',{name,room})
+   },[ENDPOINT,location.search])
     
     return <h1>Chat</h1>
 }
