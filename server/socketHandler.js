350db29a73199e224ef9043ae689ef2450e73cde
natalie <natalie@enkin.co.il>
2020-11-30 09:18:25 +0200

    modifying socket err handler

--- server/socketHandler.js
+++ server/socketHandler.js
@@ -1,5 +1,11 @@
 const socketio = require("socket.io");
 
+const errHandler = ({ err }) => {
+  if (err) {
+    console.error(`socket error: ${err}`);
+  }
+};
+
 const appio = (server) => {
   const io = socketio(server, {
     cors: {
@@ -9,15 +15,13 @@ const appio = (server) => {
   });
   io.on("connection", (socket) => {
     console.log("A new connection established");
-    socket.on("join", ({ name, room }, errHandler) => {
+    socket.on("join", ({ name, room }) => {
       console.log(`${name} joined a chat room: ${room}`);
-      if (err) {
-        errHandler({ err });
-      }
     });
     socket.on("disconnect", () => {
       console.log("user has left");
     });
+    socket.on("error", (err) => errHandler(err));
   });
 };
 
