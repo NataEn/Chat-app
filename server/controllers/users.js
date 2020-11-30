074aa509c5957f51a96976fe11804f332b80bd68
natalie <natalie@enkin.co.il>
2020-11-30 11:50:23 +0200

    mvc: switching file content

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,23 +1,6 @@
-const users = require("../models/users");
-const addUser = ({ id, name, room }) => {
-  name = name.trim().toLowerCase();
-  room = room.trim().toLowerCase();
-
-  const existingUser = users.find(
-    (user) => user.room === room && user.name === name
-  );
-  if (existingUser) {
-    return { error: "Username is taken" };
-  }
-  const user = { id, name, room };
-
-  users.push(user);
-
-  return { user };
-};
-
-const removeUser = () => {};
-
-const getUser = () => {};
-
-const getUsersInRoom = () => {};
+const {
+  addUser,
+  removeUser,
+  getUser,
+  getUsersInRoom,
+} = require("../models/users");
