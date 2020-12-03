eac6f32a025da167309bf77334ceb7003a99a6eb
natalie <natalie@enkin.co.il>
2020-12-03 09:36:30 +0200

    importing css files and creating InfoBar component

--- client/src/components/Chat/Chat.jsx
+++ client/src/components/Chat/Chat.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import queryString from "query-string";
 import io from "socket.io-client";
+import "./Chat.css"
 
 let socket;
 
