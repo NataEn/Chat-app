a77f3fc9b760d696f036e4bc396e108791f33e4e
natalie <natalie@enkin.co.il>
2020-12-05 16:17:23 +0200

    creating a user scheme

--- server/models/users.js
+++ server/models/users.js
@@ -1,1 +1,28 @@
 const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: false,
+  },
+  hobbies: {
+    type: Array,
+    required: false,
+  },
+  rooms: {
+    type: Array,
+    required: true,
+    default: ["news"],
+  },
+  subscriptionDate: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
