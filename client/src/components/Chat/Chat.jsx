e8d4ae9ce03be17347576a0b1fb8fccda62b8c64
natalie <natalie@enkin.co.il>
2020-12-10 12:09:40 +0200

    creating new input for files

--- client/src/components/Chat/Chat.jsx
+++ client/src/components/Chat/Chat.jsx
@@ -5,6 +5,8 @@ import io from "socket.io-client";
 import InfoBar from "../InfoBar/InfoBar";
 import Input from "../Input/Input";
 import Messages from "../Messages/Messages";
+import Button from "../Button/Button";
+import { uploadFile } from "../../util";
 import "./Chat.css";
 
 let socket;
@@ -16,12 +18,20 @@ const Chat = () => {
   const [users, setUsers] = useState([]);
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
+  const [inputType, setInputType] = useState(null);
+  const [file, setFile] = useState(null);
   const ENDPOINT = "localhost:5000";
 
   const sendMessage = () => {
     if (message) {
       console.log(`emitting message ${message} from client id ${socket.id}`);
       socket.emit("sendMessage", message, () => setMessage(""));
+    }
+  };
+  const sendFile = () => {
+    if (message) {
+      console.log(`emitting file ${message} from client id ${socket.id}`);
+      socket.emit("sendFile", message, () => setFile(""));
     }
   };
 
@@ -51,6 +61,11 @@ const Chat = () => {
       console.log(room, users);
       setUsers(() => users.map((user) => user.name));
     });
+    socket.on("file", (data) => {
+      console.log(data);
+      setMessages((messages) => [...messages, data]);
+      const imgTag = `<img src="data:image/png;base64, ${data}"/>`; // inject into DOM
+    });
   }, []);
   console.log(message, messages);
   return (
@@ -65,10 +80,22 @@ const Chat = () => {
             ? users.map((user, index) => <span key={index}> {user} </span>)
             : null}
         </div>
+        <img id="output"></img>
+        {/* <Button type={"text"} setInputType={setInputType} key={"text"} />
+        <Button type={"file"} setInputType={setInputType} key={"file"} /> */}
         <Input
           message={message}
           sendMessage={sendMessage}
           setMessage={setMessage}
+          type={"text"}
+        />
+        <Input
+          message={file}
+          sendMessage={sendFile}
+          setMessage={setFile}
+          type={"file"}
+          accept="image/*"
+          multiple={true}
         />
       </div>
     </div>
--- client/src/components/Input/Input.jsx
+++ client/src/components/Input/Input.jsx
@@ -1,33 +1,47 @@
-import React, { useState, useEffect } from "react";
+import "fs";
+import React from "react";
+import { uploadFile } from "../../util";
 import "./Input.css";
 
-const Input = ({ message, sendMessage, setMessage }) => (
-  <form className="form">
-    <input
-      className="input"
-      type="text"
-      placeholder="type a message..."
-      value={message}
-      onChange={(event) => setMessage(event.target.value)}
-      onKeyPress={(event) => {
-        if (event.key === "Enter") {
+const Input = ({ message, sendMessage, setMessage, type, props }) => {
+  return (
+    <form className="form" encType="multipart/form-data">
+      <input
+        {...props}
+        className="input"
+        type={type}
+        key={type}
+        placeholder="type a message..."
+        value={message}
+        onChange={(event) => {
+          console.log(event.target.value);
+          if (type === "file") {
+            setMessage(uploadFile(event));
+          } else {
+            setMessage(event.target.value);
+          }
+        }}
+        onKeyPress={(event) => {
+          if (event.key === "Enter") {
+            console.log(type, event.key);
+            event.preventDefault();
+            sendMessage(message);
+          }
+        }}
+      ></input>
+
+      <button
+        className="sendButton"
+        onClick={(event) => {
           event.preventDefault();
-          console.log("keyPress", event.key);
+          console.log("button Press", event.key);
           sendMessage(message);
-        }
-      }}
-    ></input>
-    <button
-      className="sendButton"
-      onClick={(event) => {
-        event.preventDefault();
-        console.log("button Press", event.key);
-        sendMessage(message);
-      }}
-    >
-      Send
-    </button>
-  </form>
-);
+        }}
+      >
+        Send
+      </button>
+    </form>
+  );
+};
 
 export default Input;
